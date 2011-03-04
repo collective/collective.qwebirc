33b5d2e370349d86928f69fcf72358495239f1df
Alex Clark <aclark@aclark.net>
2011-03-04 15:35:22 -0500

    Fix setup

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 
-from setuptools import find_all, setup
+from setuptools import find_packages, setup
 
 setup(
     version='1.0',
     name='collective.qwebirc',
-    packages=find_all(),
+    packages=find_packages(),
     namespace_packages=['collective'],
     install_requires='Twisted'
 )
